6bd122f568564ef72241e697091371f49c6faeab
rafael travassos <rbtravassos@gmail.com>
2024-08-02 18:35:13 -0300

    category_repository.py moved to application folder

---
+++ src/core/category/application/category_repository.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+
+class CategoryRepository(ABC):
+    @abstractmethod
+    def save(self, category):
+        raise NotImplementedError
+    
+    @abstractmethod
+    def get_by_id(self, id):
+        raise NotImplementedError
+    
+    @abstractmethod
+    def delete(self, id):
+        raise NotImplementedError    
+    
+    @abstractmethod
+    def update(self, category):
+        raise NotImplementedError
